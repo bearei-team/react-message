9c9a58f5b9851f249064bc2a6e9024e580c60ff9
Bearei <bear_ei@outlook.com>
2022-12-30 16:45:50 +0800

    feat: version 0.0.10 (#4)
    * feat: version 0.0.1
    
    * fix: fixed a timing shutdown failure
    
    * fix: fix container rendering error
    
    * feat: version 0.0.8
    
    * feat: version 0.0.9
    
    * feat: version 0.0.10

--- test/components/Message.test.tsx
+++ test/components/Message.test.tsx
@@ -1,21 +1,21 @@
-import {pickHTMLAttributes} from '@bearei/react-util';
+import { pickHTMLAttributes } from '@bearei/react-util';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Message from '../../src/components/Message';
-import {render} from '../utils/testUtils';
+import { render } from '../utils/test_utils';
 
 describe('test/components/Message.test.ts', () => {
   test('It should be a render message', async () => {
-    const {getByDataCy} = render(
+    const { getByDataCy } = render(
       <Message
         defaultVisible={true}
-        renderMain={({...props}) => (
+        renderMain={({ ...props }) => (
           <div {...pickHTMLAttributes(props)} data-cy="message">
             "message"
           </div>
         )}
-        renderContainer={({id, children}) => (
+        renderContainer={({ id, children }) => (
           <div data-cy="container" data-id={id} tabIndex={1}>
             {children}
           </div>
@@ -31,16 +31,20 @@ describe('test/components/Message.test.ts', () => {
     const user = userEvent.setup();
     let result!: boolean | undefined;
 
-    const {getByDataCy} = render(
+    const { getByDataCy } = render(
       <Message
-        onVisible={({visible}) => (result = visible)}
+        onVisible={({ visible }) => (result = visible)}
         onClick={() => {}}
-        renderMain={({onClick, ...props}) => (
-          <div {...pickHTMLAttributes(props)} data-cy="message" onClick={onClick}>
+        renderMain={({ onClick, ...props }) => (
+          <div
+            {...pickHTMLAttributes(props)}
+            data-cy="message"
+            onClick={onClick}
+          >
             "message"
           </div>
         )}
-        renderContainer={({id, children}) => (
+        renderContainer={({ id, children }) => (
           <div data-cy="container" data-id={id} tabIndex={1}>
             {children}
           </div>
